777a40cedd15814d92f95838855d694a65f63839
Al-waleed shihadeh <shihadeh@fidor.de>
2019-11-27 21:08:55 +0100

    Add Application main class

---
+++ lib/app.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+# :nodoc
+class App
+  def initialize
+    @app = Rack::Builder.new do
+      use Rack::Session::Pool, expire_after: 1000
+      use Rack::Protection
+      use GeminaboxApp::Middleware::HealthCheck
+      use GeminaboxApp::Middleware::SignUp,
+          file: "#{App.root}/config/ldap.yml"
+      use GeminaboxApp::Middleware::WebRequestsLdapAuth,
+          file: "#{App.root}/config/ldap.yml"
+      use GeminaboxApp::Middleware::ApiKey, App.ldap
+      use GeminaboxApp::Middleware::ApiGem
+
+      map '/' do
+        run Geminabox::Server
+      end
+    end
+  end
+
+  def call(env)
+    @app.call(env)
+  end
+
+  class << self
+    def root
+      Pathname.new(File.expand_path('..', __dir__))
+    end
+
+    def env
+      ENV.fetch('RACK_ENV', 'development')
+    end
+
+    def ldap
+      @ldap ||= GeminaboxApp::Ldap.new("#{App.root}/config/ldap.yml", App.env)
+    end
+  end
+end
---
+++ lib/geminabox_app.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# :nodoc
+module GeminaboxApp
+  autoload :Store,       'geminabox_app/store'
+  autoload :User,        'geminabox_app/user'
+  autoload :ApiKeyIndex, 'geminabox_app/api_key_index'
+  autoload :GemIndex,    'geminabox_app/gem_index'
+  autoload :Ldap,        'geminabox_app/ldap'
+  autoload :Middleware,  'geminabox_app/middleware'
+end
